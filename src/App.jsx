b2803fb03135e17bc6c6a6741e9141929b888778
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-11-06 20:53:53 +0100

    ✅ update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => (
-  <h1 className="text-3xl font-bold underline">Application deploy !!!</h1>
+  <h1 className="text-3xl font-bold underline">Begin Resume App</h1>
 );
 
 export default App;
