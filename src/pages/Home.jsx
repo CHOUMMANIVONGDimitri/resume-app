e057e6a2fd0f9e57dfa7de8ec9cd9ad1f3c5a07f
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-01 21:14:58 +0100

    ✅ update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Button } from "flowbite-react";
+import { useTranslation } from "react-i18next";
 import CarouselCustom from "../components/Carousel/Carousel";
+import CardCustom from "../components/Card/Card";
 
-const Home = () => (
-  <div>
-    <h2 className="py-96 text-center bg-white border-gray-200 dark:bg-gray-900 font-semibold dark:text-white">
-      Development in progress...
-    </h2>
+const Home = () => {
+  const { t } = useTranslation();
 
-    <CarouselCustom />
-
-    <img src="https://placehold.co/600x400" alt="test-placeholder" />
-    <Button>Test flowbite component</Button>
-  </div>
-);
-
+  return (
+    <div className="mt-20">
+      <CardCustom title={t("pages.home.card.title")} />
+      <CarouselCustom />
+    </div>
+  );
+};
 export default Home;
