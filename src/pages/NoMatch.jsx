b28da03fd353250a8492cd7c364b1902bbdd880c
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-01 21:44:33 +0100

    ✅ update no match page

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NoMatch = () => (
-  <div>
-    <h2 className="py-96 text-center bg-white border-gray-200 dark:bg-gray-900 font-semibold dark:text-white">
-      Page Not Found...
+  <div className="py-96">
+    <h2 className="text-center bg-white border-gray-200 dark:bg-gray-900 font-semibold dark:text-white">
+      404 Page Not Found
     </h2>
 
-    <p className="bg-white border-gray-200 dark:bg-gray-900 font-semibold dark:text-white">
+    <p className="bg-white border-gray-200 dark:bg-gray-900 font-semibold dark:text-white text-center">
       <Link to="/resume-app/">Go to the home page</Link>
     </p>
   </div>
