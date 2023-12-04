4b6fddeef56cb0899fa7a9a66c88eabd7f59aab8
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-04 11:07:35 +0100

    ✅ convert Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 import CarouselCustom from "../components/Carousel/Carousel";
 import CardCustom from "../components/Card/Card";
