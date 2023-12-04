ee360eaf1524fd4cf448375fac328ebb0b1876b1
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-04 11:04:30 +0100

    ✅ update extension and element type

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./local/i18n";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
