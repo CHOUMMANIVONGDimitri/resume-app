f3c01d41c5c6d5116f4bd19c465d70b7ee2c78ca
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-04 14:18:58 +0100

    🚨 fix lint errors

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./Layout";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Dashboard from "../pages/Dashboard";
-import NoMatch from "../pages/NoMatch";
+import Layout from "./Layout.tsx";
+import Home from "../pages/Home.tsx";
+import About from "../pages/About.tsx";
+import Dashboard from "../pages/Dashboard.tsx";
+import NoMatch from "../pages/NoMatch.tsx";
 
-const Router = () => (
+const Router: React.FC = () => (
   <Routes>
     <Route path="/resume-app/" element={<Layout />}>
       <Route index element={<Home />} />
