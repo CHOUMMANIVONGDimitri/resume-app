7017fc61f0bfce2c2f999ce19288762857b30593
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-07 21:09:49 +0100

    📈 create datas file

---
+++ src/constants/datas/index.ts
@@ -0,0 +1,24 @@
+/* datas web app */
+
+interface ContentSlide {
+  path: string;
+  alt: string;
+}
+
+const datas: ContentSlide[] =
+  [
+    {
+      path: "./images/projet-web-pokemon.jpg",
+      alt: "model figma of my web app pokemon"
+    },
+    {
+      path: "./images/eye-4-tap-bg.jpg",
+      alt: "home page of the app named Eye 4 tap"
+    },
+    {
+      path: "./images/cellsforce-app-bg.jpg",
+      alt: "presentation of cellsforce application"
+    }
+  ];
+
+export default datas;
