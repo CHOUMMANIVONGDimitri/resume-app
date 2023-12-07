91dcc7bdb238c05f704f09b7ed53f4271f3ffcff
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-07 21:11:22 +0100

    ✅ create projects page

---
+++ src/pages/Projets.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Projects: React.FC = () => (
+  <div>
+    <h2 className="py-96 text-center bg-white border-gray-200 dark:bg-gray-900 font-semibold dark:text-white">
+      Development in progress...
+    </h2>
+  </div>
+);
+export default Projects;
