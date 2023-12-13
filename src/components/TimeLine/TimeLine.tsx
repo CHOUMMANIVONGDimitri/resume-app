6f23fa821b5378add3252c7c6e8707d418861007
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-13 22:16:29 +0100

    🌐 translations on timeline component

--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 import { Moment } from "moment";
 import { Timeline, Button } from "flowbite-react";
 import { HiArrowNarrowRight, HiCalendar } from "react-icons/hi";
@@ -15,25 +17,32 @@ interface TimeLineProps {
   datas: DataTimeLine[];
 }
 
-const TimeLineCustom: React.FC<TimeLineProps> = ({ datas }) =>
-  datas?.length > 0 &&
-  datas.map((data) => (
-    <Timeline>
-      <Timeline.Item>
-        <Timeline.Point icon={data?.icon || HiCalendar} />
-        <Timeline.Content>
-          <Timeline.Time>{`${data.date.format("MMMM YYYY")}`}</Timeline.Time>
-          <Timeline.Title>{data.title}</Timeline.Title>
-          <Timeline.Body>{data.body}</Timeline.Body>
-          {data?.path && (
-            <Button color="gray">
-              Learn More
-              <HiArrowNarrowRight className="ml-2 h-3 w-3" />
-            </Button>
-          )}
-        </Timeline.Content>
-      </Timeline.Item>
-    </Timeline>
-  ));
+const TimeLineCustom: React.FC<TimeLineProps> = ({ datas }) => {
+  const { t } = useTranslation();
+
+  return (
+    datas?.length > 0 &&
+    datas.map((data) => (
+      <Timeline>
+        <Timeline.Item>
+          <Timeline.Point icon={data?.icon || HiCalendar} />
+          <Timeline.Content>
+            <Timeline.Time>{`${data.date.format("MMMM YYYY")}`}</Timeline.Time>
+            <Timeline.Title>{data.title}</Timeline.Title>
+            <Timeline.Body>{data.body}</Timeline.Body>
+            {data?.path && (
+              <Link to={`/${data.path}`}>
+                <Button color="gray">
+                  {t("buttons.view-more")}
+                  <HiArrowNarrowRight className="ml-2 h-3 w-3" />
+                </Button>
+              </Link>
+            )}
+          </Timeline.Content>
+        </Timeline.Item>
+      </Timeline>
+    ))
+  );
+};
 
 export default TimeLineCustom;
