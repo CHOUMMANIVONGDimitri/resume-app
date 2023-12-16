d5d9bf8a797f4a53a8cc43be9cf4becaf43521f8
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-12-16 14:59:33 +0100

    ✅ contact : create section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -12,51 +12,63 @@ const About: React.FC = () => {
   const forms = useRef<HTMLFormElement>(null);
 
   return (
-    <section className="my-24 flex justify-center">
-      <Card className="max-w-lg w-full md:w-2/3 lg:w-1/2">
-        <form
-          ref={forms}
-          onSubmit={(e) => sendEmail(e, forms)}
-          className="flex max-w-md flex-col gap-4"
-        >
-          <FormItem
-            name="subject"
-            label={t("pages.contact.form.subject.label")}
-            defaultValue={t("pages.contact.form.subject.default-value")}
-          />
-          <FormItem
-            name="fullName"
-            label={t("pages.contact.form.fullName.label")}
-            required
-          />
-          <FormItem
-            name="company"
-            label={t("pages.contact.form.company.label")}
-            required
-          />
-          <FormItem name="email" label={t("pages.contact.form.email.label")}>
-            <TextInput
-              id="email"
-              type="email"
-              placeholder={t("pages.contact.form.email.placeholder")}
-              required
-            />
-          </FormItem>
-          <FormItem
-            name="message"
-            label={t("pages.contact.form.message.label")}
+    <>
+      <section className="w-1/2 flex-col gap-10 justify-center mx-auto">
+        <div className="mb-5 mt-10">
+          <h1 className="mb-4 text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-4xl dark:text-white">
+            {t("pages.contact.section-1.title")}
+          </h1>
+          <p className="mb-6 text-medium font-normal text-gray-500 lg:text-lg sm:px-16 xl:px-48 dark:text-gray-400">
+            {t("pages.contact.section-1.content")}
+          </p>
+        </div>
+      </section>
+      <section className="my-24 flex justify-center">
+        <Card className="max-w-lg w-full md:w-2/3 lg:w-1/2">
+          <form
+            ref={forms}
+            onSubmit={(e) => sendEmail(e, forms)}
+            className="flex max-w-md flex-col gap-4"
           >
-            <Textarea
-              id="message"
-              placeholder={t("pages.contact.form.message.placeholfer")}
-              rows={15}
+            <FormItem
+              name="subject"
+              label={t("pages.contact.form.subject.label")}
+              defaultValue={t("pages.contact.form.subject.default-value")}
+            />
+            <FormItem
+              name="fullName"
+              label={t("pages.contact.form.fullName.label")}
               required
             />
-          </FormItem>
-          <Button type="submit">{t("buttons.submit")}</Button>
-        </form>
-      </Card>
-    </section>
+            <FormItem
+              name="company"
+              label={t("pages.contact.form.company.label")}
+              required
+            />
+            <FormItem name="email" label={t("pages.contact.form.email.label")}>
+              <TextInput
+                id="email"
+                type="email"
+                placeholder={t("pages.contact.form.email.placeholder")}
+                required
+              />
+            </FormItem>
+            <FormItem
+              name="message"
+              label={t("pages.contact.form.message.label")}
+            >
+              <Textarea
+                id="message"
+                placeholder={t("pages.contact.form.message.placeholder")}
+                rows={15}
+                required
+              />
+            </FormItem>
+            <Button type="submit">{t("buttons.submit")}</Button>
+          </form>
+        </Card>
+      </section>
+    </>
   );
 };
 
