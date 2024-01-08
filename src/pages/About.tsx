ab5c961268f78b33bbb71f666e22ba508d78fb31
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2024-01-08 22:48:13 +0100

    ✅ add hardskills section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import TimeLine from "../components/TimeLine/TimeLine.tsx";
 import Separator from "../components/Separator/Separator.tsx";
 import WorkInProgress from "../components/WorkInProgress/WorkInProgress.tsx";
+import CardCustom from "../components/Card/Card.tsx";
 
 const historyDev = [
   {
@@ -25,6 +26,44 @@ const historyDev = [
     date: moment("2023-12-01"),
     title: "Collaboration work with Saleforces",
     body: "When I was studying at Wild Code School, one of the projects to be submitted was a subject with customer needs from various companies including Salesforce who were specifying requests for an ideation platform. With the help of two other developers we created CellForce."
+  }
+];
+
+const hardSkills = [
+  {
+    name: "JavaScript",
+    description: "description ...",
+    logo: "./logo/logo-javascript.svg"
+  },
+  {
+    name: "SCSS",
+    description: "description ...",
+    logo: "./logo/logo-scss.svg"
+  },
+  {
+    name: "React JS",
+    description: "description ...",
+    logo: "./logo/logo-react.svg"
+  },
+  {
+    name: "Tailwind CSS",
+    description: "description ...",
+    logo: "./logo/logo-tailwindcss.svg"
+  },
+  {
+    name: "TypeScript",
+    description: "description ...",
+    logo: "./logo/logo-typescript.svg"
+  },
+  {
+    name: "Vite",
+    description: "description ...",
+    logo: "./logo/logo-vite.svg"
+  },
+  {
+    name: "Vue",
+    description: "description ...",
+    logo: "./logo/logo-vue.svg"
   }
 ];
 
@@ -50,6 +89,26 @@ const About: React.FC = () => {
         <Separator content="Before Become Developer" />
       </section>
       <WorkInProgress />
+      <Separator content="Hard Skills" />
+      <section>
+        <ul className="p-5 flex flex-wrap gap-5">
+          {hardSkills?.map((skill) => (
+            <li>
+              <CardCustom
+                title={skill.name}
+                content={skill.description}
+                animation
+              >
+                <img
+                  src={skill.logo}
+                  alt="javascript logo"
+                  className="h-24 w-24"
+                />
+              </CardCustom>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 };
